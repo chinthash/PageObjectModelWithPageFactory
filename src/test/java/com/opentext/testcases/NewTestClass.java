ad6b74f5253c78ff96d2e4430e90719e23ae0ebd
Shravan Chintha <schintha@opentext.com>
2021-12-14 19:27:25 +0530

    added and modified files

--- src/test/java/com/opentext/testcases/NewTestClass.java
+++ src/test/java/com/opentext/testcases/NewTestClass.java
@@ -3,6 +3,7 @@ package com.opentext.testcases;
 public class NewTestClass {
 	public static void main(String args[])
 	{
+		System.out.println("added from my branch");
 		System.out.println("This is the new testclass');
 	}
 }
