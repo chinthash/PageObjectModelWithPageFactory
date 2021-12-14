0be55e0dcc319e54f06e8a5200f1eb00c3e98727
shravan <chinthashravan@gmail.com>
2021-12-14 19:23:20 +0530

    added new line

--- src/test/java/com/opentext/testcases/NewTestClass.java
+++ src/test/java/com/opentext/testcases/NewTestClass.java
@@ -3,6 +3,8 @@ package com.opentext.testcases;
 public class NewTestClass {
 	public static void main(String args[])
 	{
-		System.out.println("This is the new testclass');
+		System.out.println("This is the new testclass");
+		System.out.println("This is the new testclass2 again added");
+		
 	}
 }
